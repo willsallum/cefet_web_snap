259ce1dcf248effbdbc77844cab88ca223c48e3b
willsallum <sallum.cefetmg@gmail.com>
2024-11-05 11:42:14 -0300

    exlcui

--- exercicio1.js
+++
@@ -1,2 +0,0 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
--- exercicio3.js
+++
@@ -1,2 +0,0 @@
-// Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
